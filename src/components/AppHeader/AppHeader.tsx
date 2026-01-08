e160952fe1dd06f7efa90f94ad8c781c21969bce
kaiyuan0708 <kaiyuan6953@gmail.com>
2026-01-08 15:10:02 +0800

    Modify Mainlayout CSS

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -4,24 +4,32 @@ import "./AppHeader.css";
 
 export function AppHeader() {
   const { i18n } = useTranslation();
+  const isEN = i18n.language === "en";
+  const isZH = i18n.language === "zh";
 
   return (
     <header>
       <nav className="nav-left">
         <NavLink to="/" className="logo-link">
-          <img
-            src="/KYTCG.png"
-            alt="KY Logo"
-            className="logo"
-          />
+          <img src="/KYTCG.png" alt="KY Logo" className="logo" />
           <span>Kayu TCG</span>
         </NavLink>
       </nav>
 
       <div className="nav-right">
-        <button onClick={() => i18n.changeLanguage("en")}>EN</button>
-        <span style={{ margin: "0 6px" }}>|</span>
-        <button onClick={() => i18n.changeLanguage("zh")}>中文</button>
+        <button
+          style={{ fontWeight: isEN ? 600 : 400 }}
+          onClick={() => i18n.changeLanguage("en")}
+        >
+          EN
+        </button>
+        <span>|</span>
+        <button
+          style={{ fontWeight: isZH ? 600 : 400 }}
+          onClick={() => i18n.changeLanguage("zh")}
+        >
+          中文
+        </button>
       </div>
     </header>
   );
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { AppHeader } from "../components/AppHeader/AppHeader";
 import { AppFooter } from "../components/AppFooter/AppFooter";
-import "./MainLayout.css";
 
 export function MainLayout() {
   return (
